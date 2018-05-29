51352892ab401f830614bc746e1a89a1396100ed
cyruszevirus <cyrille.gattiker@gmail.com>
2018-05-29 19:24:18 +0800

    Script replacing EPFL with PINEAPPLEPFL in the strings of the texts, yet keeps the links working

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$html = file_get_contents('https://www.epfl.ch/');
+$html = str_replace('href="//', 'href="https://', $html);
+$html = str_replace('src="/', 'src="https://www.epfl.ch/', str_replace('href="/', 'href="https://www.epfl.ch/', $html));
+$html = str_replace('<link href="https://www.epfl.ch/public/hp2013/css/app.e49decc256e9.css" rel="stylesheet" type="text/css" />', '<style>'.file_get_contents('https://www.epfl.ch/public/hp2013/css/app.e49decc256e9.css').'</style>', $html); 
+$html = str_replace('../epfl-bootstrap/fonts/epfl-icons.8d666797fa9a.woff', '/fonts/epfl-icons.8d666797fa9a.woff', $html);
+
+$html = str_replace('<head>', '<!--
+
+       .__                                   .__  ________________________________.____     
+______ |__| ____   ____ _____  ______ ______ |  | \_   _____/\______   \_   _____/|    |    
+\____ \|  |/    \_/ __ \\__  \ \____ \\____ \|  |  |    __)_  |     ___/|    __)  |    |    
+|  |_> >  |   |  \  ___/ / __ \|  |_> >  |_> >  |__|        \ |    |    |     \   |    |___ 
+|   __/|__|___|  /\___  >____  /   __/|   __/|____/_______  / |____|    \___  /   |_______ \
+|__|           \/     \/     \/|__|   |__|                \/                \/            \/
+
+--><head>', $html);
+
+
+$html = str_replace('EPFL', '🍍PINEAPPEPFL🍍', $html);
+echo ($html);
+?>
